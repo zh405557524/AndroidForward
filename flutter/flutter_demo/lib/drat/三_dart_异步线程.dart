28adc8e900b28d0f858b12787f7a917ddac3df13
zhuming <405557524@qq.com>
2025-03-05 19:19:50 +0800

    feat:异步线程-事件

--- flutter/flutter_demo/lib/drat/三_dart_异步线程.dart
+++ flutter/flutter_demo/lib/drat/三_dart_异步线程.dart
@@ -1,13 +1,29 @@
 import 'dart:io';
+import 'dart:isolate';
+
+var i = 0;
 
 void main() {
-  // readFile().then(onValue);
+  //事件循环
+  i = 100;
+  //事件接收器
+  var receivePort = new ReceivePort();
+  Isolate.spawn(entryPoint, receivePort.sendPort);
+  receivePort.listen((t) {
+    print(t);
+  });
+
+  //微队列
+}
+
+void entryPoint(SendPort sendPort) {
+  i = 200;
+  sendPort.send("Hello from entryPoint");
 }
 
 Future<String> readFile() async {
   String content = await new File(
-      r"D:\work\code\soul\AndroidForward\flutter\flutter_demo\lib\drat\a.txt")
-      .readAsStringSync();
-
+          r"D:\work\code\soul\AndroidForward\flutter\flutter_demo\lib\drat\a.txt")
+      .readAsString();
   return "content:${content}";
 }
