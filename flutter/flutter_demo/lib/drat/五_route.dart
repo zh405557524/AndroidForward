f95c4d20ea738523d1450272d2e53b3a0fdb529d
zhuming <405557524@qq.com>
2025-03-31 20:06:59 +0800

    feat:route

--- flutter/flutter_demo/lib/drat/五_route.dart
+++ flutter/flutter_demo/lib/drat/五_route.dart
@@ -6,13 +6,14 @@ void main() => runApp(MainPage());
 class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(routes: {
-      "/": (_) {//默认首页 路由地址,
-        return MainPageDetail();
+    return MaterialApp(
+      routes: {
+        //默认首页 路由地址,
+        "/": (context) => MainPageDetail(),
+        "/page2": (context) => Page2()
       },
-      "/page2": (context) => Page2()
-    }, title: "第一个页面",
-        // home: MainPageDetail()//如果有默认首页，就不需要home
+      title: "第一个页面",
+      // home: MainPageDetail()//如果有默认首页，就不需要home
     );
   }
 }
@@ -24,22 +25,46 @@ class MainPageDetail extends StatelessWidget {
       appBar: AppBar(
         title: Text("第一个页面"),
       ),
-      body: Column(
-        children: <Widget>[
-          ElevatedButton(
-              onPressed: () async {
-                // Lance lance = await
-                // Navigator.push(context,
-                //     MaterialPageRoute(builder: (context) {
-                //   return Page2();
-                // }));
-                Navigator.pushNamed(context, "/page2");
-                // print("lance:${lance.name}");
-              },
-              child: Text("跳转到第二个页面")),
-        ],
+      body: Builder(
+        builder: (context) {
+          return Column(
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: () {
+                  startNavigator(context);
+                },
+                child: Text("跳转到第二个页面"),
+              ),
+            ],
+          );
+        },
       ),
     );
+  }
+
+  void startNavigator(context) {
+    //跳转 添加动画
+    // Navigator.push(context, MaterialPageRoute(builder: (context) {
+    //   return Page2();
+    // }));
+
+    // Navigator.pushNamed(context, "/page2");
+
+    Navigator.push(
+        context,
+        PageRouteBuilder(
+            transitionDuration: Duration(microseconds: 500),
+            pageBuilder: (BuildContext context, Animation<double> animation,
+                Animation<double> secondaryAnimation) {
+              return SlideTransition(
+                ///Tween:在补间动画中，定义开始点结束点
+                position: new Tween<Offset>(
+                  begin: const Offset(1.0, 0.0),
+                  end: const Offset(0.0, 0.0),
+                ).animate(animation),
+                child: Page2(),
+              );
+            }));
   }
 }
 
